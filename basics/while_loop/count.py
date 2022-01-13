d070627a7d8ac051c46a029edf0788da3695265e
Tony Livins <5livit20@solent.ac.uk>
2022-01-13 02:31:05 +0000

    .

---
+++ basics/while_loop/count.py
@@ -0,0 +1,5 @@
+counter = int (input("How many live cables should I avoid?"))
+counter1 = 1
+while (counter >= counter1):
+    print(f"Avoiding...Done! {counter1} live cables avoided.")
+    counter1 = counter1 + 1
---
+++ basics/while_loop/simple.py
@@ -0,0 +1,4 @@
+counter = int(input("How many cables should I remove?\n"))
+while(counter > 0):
+    counter = counter - 1
+    print ("Removed cable.")
